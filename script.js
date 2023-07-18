781b08e29dad79b6b09e600720553b7bf4ed9098
Yana Mishyna <yana.mishina.92@gmail.com>
2023-07-18 18:33:24 -0400

    defined a function for genPass, created promt for user input and created messages for promt

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Define Generate Password function 
-function generatePassword () { 
+// Define Generated Password function 
+function generatePassword() {
 
+  // Create var to hold password
+  var password = '';
+
+  // 1. promt user for the pasword creteria
+  //    a. password length 8 < 128
+  var length = prompt('How long do you want your password to be?')
+  if (length < 8 || length > 128) {
+    alert('Please enter between 8 and 128.');
+    return null;
+  }
+
+  //    b. lowercase, uppercase, numbers, specia, characters
+  var lower = confirm ('Click OK to include lowercase character.');
+  var upper = confirm ('Click OK to include uppercase character.');
+  var numeric = confirm ('Click OK to include numeric characters.');
+  var specialChar = confirm ('Click OK to include special characters.');
+
+  // Arrays holding different combinsations
+
+  
+
+
+  // 2. validate the input 
+  if (lower === false && upper === false && numeric === false && specialChar === false) {
+    alert ('Please choose at least one character type.');
+    return null;
+  }
+
+
+  // 3. generate password based on criteria
+
+
+
+
+
+  // 4. display password to the page 
+
+  return 'Generated password will go here!'
+}
 
 // Write password to the #password input
 function writePassword() {
